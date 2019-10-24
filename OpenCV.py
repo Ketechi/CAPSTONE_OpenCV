cfd61bb3473b0975592fc8de7997be844cf8a419
Ketechi <ketechimelorudo@gmail.com>
2019-10-24 12:22:15 -0400

    Finds center of shapes with thresholes and sizes

--- OpenCV.py
+++ OpenCV.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
-import imutils
+import pyautogui
 
 # Variables
+'''
 red = 47
 green = 93
 blue = 66
@@ -10,12 +11,15 @@ color = red, green, blue
 minRange_color = color[0] - 30, color[1] - 30, color[2] - 30
 maxRange_color = color[0] + 30, color[1] + 30, color[2] + 30
 
+
 # Functions
 #-----------------------------------------
 def colorPick(colorR, colorG, colorB):
     return np.array([colorR, colorG, colorB])
 
 #-----------------------------------------
+'''
+
 
 # Selects the video Source
 cap = cv2.VideoCapture(1)
@@ -25,8 +29,10 @@ while True:
     extra, frames = cap.read()
 
     colorMap = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+
     # mask = cv2.inRange(colorMap, colorPick(minRange_color[0], minRange_color[1], minRange_color[2]), colorPick(maxRange_color[0], maxRange_color[1], maxRange_color[2]))
-    ret, thresh = cv2.threshold(colorMap, 127, 255, 0)
+
+    ret, thresh = cv2.threshold(colorMap, 127, 255, 0) # part 2
     contours, extra = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     # Capture each contour and the center of contours
@@ -42,17 +48,23 @@ while True:
 
         # Draw the center and the contour
         drawArea = cv2.contourArea(contour)
-        if drawArea > 500 and drawArea < 3000:
+        if drawArea > 500 and drawArea < 10000:
             cv2.drawContours(frames, contour, -1, (0, 255, 0), 1)
-            cv2.circle(frames, (momentumX, momentumY), 1, (0, 255, 0), -1)
+            cv2.circle(frames, (momentumX, momentumY), 5, (0, 255, 0), -1)
+            print(momentumX , momentumY)
+
+            # part 3
+            '''
+            if cv2.waitKey(1) == 97: # a button
+                print(momentumX, momentumY)
+                pyautogui.click(momentumX, momentumY)
+            '''
 
     cv2.imshow("Video", frames)
 
     # Stop the video capturing loop
-    if cv2.waitKey(1) == 27:
+    if cv2.waitKey(1) == 27: # ESC button
         break
-    elif cv2.waitKey(1) == 122: # Run a color picker
-        colorPick()
 
 
 # Eradicate the script and close the script
